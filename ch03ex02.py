201b8cab99c1dc2ff086d7d7a2faa87cd46c09e5
Juyeong <jylee840424@gmail.com>
2025-12-31 18:18:04 +0900

    HTML_CSS_JS

---
+++ ch03ex02.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as pd
+
+url = "https://hn.algolia.com/api/v1/search_by_date"
+params = {
+    "query": "ai",
+    "tags": "story"
+}
+
+response = requests.get(url, params=params)
+json_data = response.json()
+
+for article in json_data["hits"]:
+    title = article.get("title")
+    date = article.get("created_at")
+    content = article.get("story_text")
+    print(title, date)
+
+
+
